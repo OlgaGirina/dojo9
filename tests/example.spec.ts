77ecac3e6ffee6fab29f5e70d1beb5d41ac8b19d
HudenkoO <hudenko@Olgas-MacBook-Air-2.local>
2026-06-22 09:34:26 +0200

    first commit

---
+++ tests/example.spec.ts
@@ -0,0 +1,103 @@
+import { test, expect, request } from "@playwright/test";
+
+test("get products - should be successful", async ({ request }) => {
+  const response = await request.get("/api/v1/products");
+
+  expect(response).toBeOK();
+  expect(response.status()).toBe(200);
+});
+
+test("create products - should be successful", async ({ request }) => {
+  const randomNum = Math.floor(Math.random() * 1_000_000);
+  await request.post("/api/v1/products/", {
+    data: {
+      title: "New Product" + randomNum,
+      price: 10,
+      description: "A description",
+      categoryId: 1,
+      images: ["https://placehold.co/600x400"],
+    },
+  });
+});
+
+test("update products - should be successful", async ({ request }) => {
+  const randomNum = Math.floor(Math.random() * 1_000_000);
+  const response = await request.post("/api/v1/products/", {
+    data: {
+      title: "New Product" + randomNum,
+      price: 10,
+      description: "A description",
+      categoryId: 1,
+      images: ["https://placehold.co/600x400"],
+    },
+  });
+  const jsonCreate = await response.json();
+  const productId = jsonCreate.id;
+
+  await request.put(`/api/v1/products/${productId}`, {
+    data: {
+      title: "New Updated Product",
+      price: 10,
+      description: "Updated description ",
+      categoryId: 1,
+      images: ["https://placehold.co/600x400"],
+    },
+  });
+});
+test("delete products - should be successful", async ({ request }) => {
+  const randomNum = Math.floor(Math.random() * 1_000_000);
+  const response = await request.post("/api/v1/products/", {
+    data: {
+      title: "New Product" + randomNum,
+      price: 10,
+      description: "A description",
+      categoryId: 1,
+      images: ["https://placehold.co/600x400"],
+    },
+  });
+  const jsonDelete = await response.json();
+  const productIdDelete = jsonDelete.id;
+
+  await request.delete(`/api/v1/products/${productIdDelete}`);
+});
+
+test("check pagination", async ({ request }) => {
+  const response = await request.get("/api/v1/products", {
+    params: {
+      offset: 0,
+      limit: 10,
+    },
+  });
+  const json = await response.json();
+
+  expect(response.status()).toBe(200);
+  expect(json.length).toBeLessThanOrEqual(10);
+  expect(json[0]).toHaveProperty("id");
+});
+
+test("get products by id - should be successful", async ({ request }) => {
+  const randomNum = Math.floor(Math.random() * 1_000_000);
+  const response = await request.post("/api/v1/products/", {
+    data: {
+      title: "New Product" + randomNum,
+      price: 10,
+      description: "A description",
+      categoryId: 2,
+      images: ["https://placehold.co/600x400"],
+    },
+  });
+  const json = await response.json();
+  const id = json.id;
+  const idRelated = await request.get(`/api/v1/products/${id}/related`);
+  expect(idRelated.status()).toBe(200);
+});
+
+test("get product related by slug", async ({ request }) => {
+  const slugRelated = await request.get(
+    "api/v1/products/slug/handmade-fresh-table/related",
+  );
+  const json = await slugRelated.json();
+  expect(
+    json.every((item) => item.category.slug === "handmade-fresh-table"),
+  ).toBeTruthy();
+});
